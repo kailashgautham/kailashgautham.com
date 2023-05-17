2ab05529ec7a9f67bab64010e8593d9c3a82b572
kailashgautham <kailash.gautham@gmail.com>
2023-05-17 16:32:27 +0700

    added css modules and global CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,13 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import Script from 'next/script';
-
+import Layout from '../../components/layout';
 export default function FirstPost() {
     return (
-        <div>
+        <Layout>
             <Head>
                 <title>My First Post</title>
             </Head>
             <h1>First Post</h1>
-            <h2><Link href="/">Back to home</Link></h2>
-        </div>
+        </Layout>
     );
 }
