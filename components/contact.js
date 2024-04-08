661982d7af75fe11901cf5bcb4aa324470434e04
kailashgautham <kailash.gautham@gmail.com>
2024-04-08 11:59:26 +0800

    website changes

--- components/contact.js
+++ components/contact.js
@@ -6,8 +6,8 @@ function Contact() {
             <h1>Contact Me</h1><br></br>
             <p>Feel free to reach out to me using the information below:</p>
             <ul>
-                <li>LinkedIn: <a href="https://linkedin.com/in/kailashgautham">linkedin.com/in/kailashgautham</a></li>
-                <li>GitHub: <a href="https://github.com/kailashgautham">github.com/kailashgautham</a></li>
+                <li>LinkedIn: <a target="_blank" href="https://linkedin.com/in/kailashgautham">linkedin.com/in/kailashgautham</a></li>
+                <li>GitHub: <a target="_blank" href="https://github.com/kailashgautham">github.com/kailashgautham</a></li>
                 <li>Email: kailash dot gautham at gmail dot com</li>
             </ul>
         </div>
