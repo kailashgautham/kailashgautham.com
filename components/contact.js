da8677a6952293cb771224d01735ff7e12bf0613
kailashgautham <kailash.gautham@gmail.com>
2024-04-10 11:58:58 +0800

    made changes

--- components/contact.js
+++ components/contact.js
@@ -3,12 +3,13 @@ import React from 'react';
 function Contact() {
     return (
         <div>
-            <h1>Contact Me</h1><br></br>
-            <p>Feel free to reach out to me using the information below:</p>
+            <h1>Contact</h1><br></br>
+            <p>Here are some of my portfolio and personal websites, feel free to reach out!</p>
             <ul>
                 <li>LinkedIn: <a target="_blank" href="https://linkedin.com/in/kailashgautham">linkedin.com/in/kailashgautham</a></li>
                 <li>GitHub: <a target="_blank" href="https://github.com/kailashgautham">github.com/kailashgautham</a></li>
-                <li>Email: kailash dot gautham at gmail dot com</li>
+                <li>SoundCloud: <a target="_blank" href="https://soundcloud.com/kailash-gautham">soundcloud.com/kailash-gautham</a></li>
+                <li>Email: kailash[dot]gautham[at]gmail[dot]com</li>
             </ul>
         </div>
     );
